8e31298442a1e0516eec18dc5bf77f349d963b66
Benton Snyder <benton.snyder@gmail.com>
2015-07-02 20:48:42 -0500

    updated

--- sugarcrm.py
+++ sugarcrm.py
@@ -1,47 +1,74 @@
 """
 Python Library for SugarCRM SOAP API
 
-    http://support.sugarcrm.com/02_Documentation/04_Sugar_Developer/Sugar_Developer_Guide_6.5/02_Application_Framework/Web_Services/
+    http://support.sugarcrm.com/02_Documentation/04_Sugar_Developer/Sugar_Developer_Guide_6.7/02_Application_Framework/Web_Services/
 
 Author: Benton Snyder
 Website: http://bensnyde.me
 Created: 5/19/13
-Updated: 3/13/15
+Updated: 7/2/15
 
 """
-import hashlib
-import base64
 from suds.client import Client
+from suds.plugin import MessagePlugin
+import hashlib
 
-class SugarCrm:
+class SugarCRM:
     def __init__(self, url, username, password, application="default"):
         self._client = Client(url)
-        response = self._client.service.login({"user_name":username, "password":hashlib.md5(password).hexdigest()}, application)
-        self._session = response.id
+
+        auth = self._client.factory.create("user_auth")
+        auth.user_name = username
+        auth.password = hashlib.md5(password).hexdigest()
+
+        self._session = self._client.service.login(auth, "default")
 
     # Returns server information such as version, flavor, and gmt_time
     def get_server_info(self):
-        return self._client.service.get_server_info()
+        return self._client.service.get_server_info(self._session)
 
     # Returns the ID of the user who is logged into the current session
     def get_user_id(self):
         return self._client.service.get_user_id(self._session)
 
+    # Retrieves a list of SugarBean
+    #    ex. sugar.get_entry_list('Contacts')
+    def get_entry_list(self, module_name, query="", order_by="", offset=0, select_fields=[], link_name_to_fields={}, max_results=0, deleted=0, favorites=0):
+        return self._client.service.get_entry_list(self._session, module_name, query, order_by, offset, select_fields, link_name_to_fields, max_results, deleted, favorites)
+
     # Retrieves a single SugarBean based on ID
+    #    ex. sugar.get_entry('Contacts', '2561916f-c033-c446-2dad-55955fe1402e')
     def get_entry(self, module_name, id, select_fields=[], link_name_to_fields={}):
         return self._client.service.get_entry(self._session, module_name, id, select_fields, link_name_to_fields)
 
     # Retrieves a list of SugarBeans based on the specified IDs
+    #    ex. sugar.get_entry('Contacts', ['2561916f-c033-c446-2dad-55955fe1402e']. ['first_name', 'last_name'])
     def get_entries(self, module_name, ids=[], select_fields=[], link_name_to_fields={}):
         return self._client.service.get_entries(self._session, module_name, ids, select_fields, link_name_to_fields)
 
-    # Retrieves a list of SugarBeans
-    def get_entry_list(self, module_name, query="", order_by="", offset=0, select_fields=[], link_name_to_fields={}, max_results=0, deleted=0, favorites=0):
-        return self._client.service.get_entry_list(self._session, module_name, query, order_by, offset, select_fields, link_name_to_fields, max_results, deleted, favorites)
+    # Retrieves the list of modules available to the current user logged into the system
+    def get_available_modules(self, filter="default"):
+        if filter not in ["default", "all", "mobile"]:
+            raise Exception("Invalid filter specified to getAvailableModules().")
+
+        return self._client.service.get_available_modules(self._session, filter)
 
     # Retrieves the specified number of records in a module
     def get_entries_count(self, module_name, query="", deleted=0):
         return self._client.service.get_entries_count(self._session, module_name, query, deleted)
+
+    # Retrieves variable definitions for fields of the specified SugarBean
+    def get_module_fields(self, module_name, fields=[]):
+        return self._client.service.get_module_fields(self._session, module_name, fields)
+
+    # Retrieves a list of recently viewed records by module for the current user
+    def get_last_viewed(self, module_names):
+        return self._client.service.get_last_viewed(self._session, module_names)
+
+    # Allows authenticated user with appropriate permission to download a document
+    def get_document_revision(self, id):
+        return self._client.service.get_document_revision(self._session, id)
+
 
     # Sets a single relationship between two SugarBeans
     def set_relationship(self, module_name, module_id, link_field_name, related_ids=[], name_value_list={}, delete=0):
@@ -80,29 +107,11 @@ class SugarCrm:
 
         return self._client.service.set_document_revision(self._session)
 
-    # Allows authenticated user with appropriate permission to download a document
-    def get_document_revision(self, id):
-        return self._client.service.get_document_revision(self._session, id)
-
-    # Returns the ID, module_name and fields for the specified modules
-    def search_by_module(self, search_string, modules, offset=0, max_results=0, assigned_user_id="", select_fields=[], unified_search_only=0, favorites=0):
-        return self._client.service.search_by_module(self._session, search_string, modules, offset, max_results, assigned_user_id, select_fields, unified_search_only, favorites)
-
-    # Retrieves the list of modules available to the current user logged into the system
-    def get_available_modules(self, filter="default"):
-        if filter not in ["default", "all", "mobile"]:
-            raise Exception("Invalid filter specified to getAvailableModules().")
-
-        return self._client.service.get_available_modules(self._session, filter)
-
-    # Retrieves the ID of the default team of the user who is logged into the current session
-    def get_user_team_id(self):
-        return self._client.service.get_user_team_id(self._session)
-
     # Performs a mail merge for the specified campaign
     def set_campaign_merge(self, campaign_id, targets=[]):
         return self._client.service.set_campaign_merge(self._session, targets, campaign_id)
 
-    # Retrieves variable definitions for fields of the specified SugarBean
-    def get_module_fields(self, module_name, fields=[]):
-        return self._client.service.get_module_fields(self._session, module_name, fields)
+
+    # Returns the ID, module_name and fields for the specified modules
+    def search_by_module(self, search_string, modules, offset=0, max_results=0, assigned_user_id="", select_fields=[], unified_search_only=0, favorites=0):
+        return self._client.service.search_by_module(self._session, search_string, modules, offset, max_results, assigned_user_id, select_fields, unified_search_only, favorites)
